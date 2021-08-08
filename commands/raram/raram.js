cb5242fb6b0fc7d48cbd8f261e745412d5f0aac6
Marco <marcoverbeek99@gmail.com>
2021-08-08 16:56:21 +0200

    Created function that allows players to analyse their last ARAM game by writing !raram

--- commands/raram/raram.js
+++ commands/raram/raram.js
@@ -5,33 +5,48 @@ const { champ_icon_or_random, random_rank_icon } = require('../../utils/champion
 const axios = require('axios')
 const commands = require('../../data/commands.json')
 
+async function command_analyse_last_game(msg) {
+  // axios request get player last game id
+  // use last game id in command_analyse
+  return command_analyse(msg, '5397055718')
+}
+
 /**
- * Displays an analysis of the player's last game || gameId.
+ * Displays an analysis of the league game with provided ID.
  * @param {CommandoMessage} message - The message the command is being run for
  */
-async function command_analyse(msg, gameId = 5399406298) {
-  // axios request on endpoint /analyse/:gameId||:discordId
-  // returns a message 'success'/'error' and the amount of total games in queue
+async function command_analyse(msg, gameId) {
+  try {
+    const res = await axios.get('http://localhost:3000/analyses/' + gameId)
 
-  //const res = await axios.get('http://localhost:5000/api/analysis/only/' + gameId).data
-  const res = require('../../data/example_game.json')
-  const players = res["players"]
-  let col1 = "", col2 = "", col3 = ""
+    const players = res["data"]["players"]
+    let col1 = "", col2 = "", col3 = ""
 
-  for(var i=0; i<5; i++){
-    col1 += champ_icon_or_random(players[i]["champion"]) + " | " + players[i]["summonerName"] + "\n";
-    col2 += players[i]["kills"] + "/" + players[i]["deaths"] + "/" + players[i]["assists"] + "\n";
-    col3 += random_rank_icon() + " ("+ (players[i]["lpGain"] >= 0 ? "+" : "" ) + players[i]["lpGain"] + ")\n";
+    for (var i = 0; i < 5; i++) {
+      col1 += champ_icon_or_random(players[i]["champion"]) + " | " +
+        players[i]["summonerName"] + "\n";
+      col2 += `[${players[i]["kills"]}/${players[i]["deaths"]}/${players[i]["assists"]}](${msg.url} "Damage Done: 12245\nDamage Taken: 32422 \nHealed: 1200")` +
+        "\n";
+      col3 += random_rank_icon() + " (" +
+        (players[i]["lpGain"] >= 0 ? "+" : "") + players[i]["lpGain"] + ")\n";
+    }
+
+    const embed = new MessageEmbed().setAuthor(
+      "Here are your rARAM stats for your last played ARAM:",
+      msg.author.displayAvatarURL()).
+      setColor(0x00b3ff).
+      addField("Player", col1, true).
+      addField("K/D/A", col2, true).
+      addField("Rank", col3, true)
+
+    return msg.embed(embed);
+  } catch (e) {
+    const embed = new MessageEmbed()
+      .setAuthor("An error occured. rARAM could not find the game with specified id.\nAre you sure this is an existing ARAM game?")
+      .setColor(0xFF0000)
+
+    return msg.embed(embed);
   }
-
-  const embed = new MessageEmbed()
-  .setAuthor("Here are your rARAM stats for your last played ARAM:", msg.author.displayAvatarURL())
-  .setColor(0x00b3ff)
-  .addField("Player", col1, true)
-  .addField("K/D/A", col2, true)
-  .addField("Rank", col3, true)
-
-  return msg.embed(embed);
 }
 
 /**
